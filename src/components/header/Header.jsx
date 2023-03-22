c7a40198ca3494c865465b5f35798033f29aa3d2
Jefferson Pulido <jeffersonpulido55@mail.com>
2023-03-21 20:14:19 -0500

    Header v1 Input Search

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,8 +5,14 @@ export const Header = () => {
     return (
         <>
             <div className="containerTitle">
-                <i class="fa-solid fa-magnifying-glass"></i><h1>Searcher Super<span>heroes</span></h1>
+                <div className='title'>
+                    <h1>Searcher Super<span>heroes</span></h1>
+                </div>
+                <div className='containerSearch'>
+                    <input type="text" name='nameField' className='inputField' placeholder='Ingresa aqui el nombre de tu superheroe...' />
+                </div>
             </div>
+
         </>
     )
 }
