df44eb2b6f32990eb3a4ea8777d0cee35a28574b
Jefferson Pulido <jeffersonpulido55@mail.com>
2023-03-21 20:01:14 -0500

    Header v1

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,6 +4,9 @@ import './Header.css'
 export const Header = () => {
     return (
         <>
+            <div className="containerTitle">
+                <i class="fa-solid fa-magnifying-glass"></i><h1>Searcher Super<span>heroes</span></h1>
+            </div>
         </>
     )
 }
