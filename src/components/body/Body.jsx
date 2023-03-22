184296c425b1075de969c7e2f1668a0e770fe473
Jefferson Pulido <jeffersonpulido55@gmail.com>
2023-03-22 14:37:59 -0500

    Cards Heroes v1.2 body

--- src/components/body/Body.jsx
+++ src/components/body/Body.jsx
@@ -5,7 +5,7 @@ export const Body = () => {
   const [heroes, setHeroes] = useState([]);
 
   useEffect(() => {
-    fetch("https://www.superheroapi.com/api.php/812629123100283/search/ironman")
+    fetch("https://www.superheroapi.com/api.php/812629123100283/search/hulk")
       .then((response) => response.json())
       .then((data) => setHeroes(data.results))
       .catch((error) => console.log(error));
@@ -14,49 +14,74 @@ export const Body = () => {
   return (
     <div className="containerData">
       {heroes.map((heroe) => (
-        <div className="cardData">
+        <div className="cardData" data-aos="flip-left" data-aos-delay="100">
           <div className="headData">
-            <p key={heroe.id} className="nameHeroe">
-              {heroe.name}
-            </p>
+            <p key={heroe.id} className="nameHeroe"> {heroe.name} </p>
             <p className="fullnameHeroe">{heroe.biography["full-name"]}</p>
           </div>
           <div className="imgData">
-            <img
-              src={heroe.image["url"]}
-              alt="ImgHeroe"
-              className="imgPortrait"
-            />
+            <img data-aos="fade-up" data-aos-delay="300" src={heroe.image["url"]} alt="ImgHeroe" className="imgPortrait" />
           </div>
           <div className="powerstatsData">
             <p className="powerTitle"><i class="fa-solid fa-shield-halved"></i> PowerStats <i class="fa-solid fa-shield-halved"></i></p>
             <div className="powerDesc">
-              <div className="powerItem">
+              <div className="powerItem" data-aos="flip-left" data-aos-delay="300">
                 <p>Intelligence</p>
                 <span>{heroe.powerstats["intelligence"]}</span>
               </div>
-              <div className="powerItem">
+              <div className="powerItem" data-aos="flip-left" data-aos-delay="300">
                 <p>Strength</p>
                 <span>{heroe.powerstats["strength"]}</span>
               </div>
-              <div className="powerItem">
+              <div className="powerItem" data-aos="flip-left" data-aos-delay="300">
                 <p>Speed</p>
                 <span>{heroe.powerstats["speed"]}</span>
               </div>
-              <div className="powerItem">
+              <div className="powerItem" data-aos="flip-left" data-aos-delay="300">
                 <p>Durability</p>
                 <span>{heroe.powerstats["durability"]}</span>
               </div>
-              <div className="powerItem">
+              <div className="powerItem" data-aos="flip-left" data-aos-delay="300">
                 <p>Power</p>
                 <span>{heroe.powerstats["power"]}</span>
               </div>
-              <div className="powerItem">
+              <div className="powerItem" data-aos="flip-left" data-aos-delay="300">
                 <p>Combat</p>
                 <span>{heroe.powerstats["combat"]}</span>
               </div>
             </div>
           </div>
+          <hr className="hr"/>
+          <div className="appearanceData">
+            <p className="powerTitle"><i class="fa-solid fa-star"></i> Appearance <i class="fa-solid fa-star"></i></p>
+            <div className="powerDesc">
+              <div className="powerItem" data-aos="flip-right" data-aos-delay="600">
+                <p>Gender</p>
+                <span>{heroe.appearance["gender"]}</span>
+              </div>
+              <div className="powerItem" data-aos="flip-right" data-aos-delay="600">
+                <p>Race</p>
+                <span>{heroe.appearance["race"]}</span>
+              </div>
+              <div className="powerItem" data-aos="flip-right" data-aos-delay="600">
+                <p>Height</p>
+                <span>{heroe.appearance["height"][1]}</span>
+              </div>
+              <div className="powerItem" data-aos="flip-right" data-aos-delay="600">
+                <p>Weight</p>
+                <span>{heroe.appearance["weight"][1]}</span>
+              </div>
+              <div className="powerItem" data-aos="flip-right" data-aos-delay="600">
+                <p>Eye Color</p>
+                <span>{heroe.appearance["eye-color"]}</span>
+              </div>
+              <div className="powerItem" data-aos="flip-right" data-aos-delay="600">
+                <p>Hair Color</p>
+                <span>{heroe.appearance["hair-color"]}</span>
+              </div>
+            </div>
+          </div>
+          <hr className="hr"/>
         </div>
       ))}
     </div>
